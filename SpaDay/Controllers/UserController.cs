c87ef4567421a9a327f7ab5b9fbe5a5e22823c9e
JustynBougeno <93269509+JustynBougeno@users.noreply.github.com>
2021-12-16 12:03:31 -0600

    Learning about Models

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+
+namespace SpaDay.Controllers 
+{
+    public class UserController : Controller
+    {
+        [Route("/user/add")]
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            
+            if (newUser.Password == verify)
+            {
+                ViewBag.userName = newUser.UserName;
+                return View("index");
+            }
+            else
+            {
+                return Redirect("/user/");
+            }
+        }
+
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; }
+
+        public string VerifyPasswed { get; }
+
+
+        public User(string userName, string email, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
